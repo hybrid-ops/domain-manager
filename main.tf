99b296b7437278209fb3d4d3e7cbbdd5f9956dcb
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-12-16 13:49:43 +0000

    baseline aws and az root domains

--- main.tf
+++ main.tf
@@ -12,18 +12,6 @@ module "rootzone" {
   basedomain = var.basedomain
 }
 
-module "testsubdomain1" {
-  source       = "./aws_zone"
-  name         = "testzone1.${var.basedomain}"
-}
-
-module "testsubdomain1ns" {
-  source       = "./aws_zone_ns"
-  name         = "testzone1.${var.basedomain}"
-  root_zone_id = module.rootzone.zone_id
-  name_servers = module.testsubdomain1.name_servers
-}
-
 module "azurezone" {
   source       = "./azure_zone"
   name         = "az.${var.basedomain}"
@@ -36,9 +24,21 @@ module "azurezonens" {
   name_servers = module.azurezone.name_servers
 }
 
+module "awsbase" {
+  source        = "./aws_zone"
+  name          = "aws.${var.basedomain}"
+}
+
+module "awsbasens" {
+  source       = "./aws_zone_ns"
+  name         = "aws.${var.basedomain}"
+  root_zone_id = module.rootzone.zone_id
+  name_servers = module.awsbase.name_servers  
+}
+
 output "available_zones" {
   value = [
-    module.testsubdomain1ns.fqdn,
+    module.awsbasens.fqdn,
     module.azurezonens.fqdn
   ]
 }
