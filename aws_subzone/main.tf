905212eeaae653cc0aba0bc946d03f7cad432089
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-12-18 11:14:49 +0000

    add subzone support modules

---
+++ aws_subzone/main.tf
@@ -0,0 +1,40 @@
+variable "name" { }
+
+variable "comment" {
+  default = "Managed by SRE Domain Manager"
+}
+
+variable "parent_zone" { }
+
+variable "tags" {
+  type    = map
+  default = { }
+}
+
+# Create the hosted zone
+resource "aws_route53_zone" "z" {
+    name    = var.name
+    comment = var.comment
+    tags    = var.tags
+}
+
+# Create the zone NS Records in the parent zone
+resource "aws_route53_record" "zns" {
+  name = var.name
+  type = "NS"
+  ttl = 3600 # 1 hour ttl for these entries
+  zone_id = var.parent_zone
+  records = aws_route53_zone.z.name_servers
+}
+
+output "fqdn" {
+  value = aws_route53_record.zns.fqdn
+}
+
+output "zone_id" { 
+  value = aws_route53_zone.z.zone_id 
+}
+
+output "name_servers" {
+  value = aws_route53_zone.z.name_servers
+}
---
+++ azure_subzone/main.tf
@@ -0,0 +1,41 @@
+variable "name" { }
+
+variable "resource_group" {
+  default = "sre-domain-manager"
+}
+
+variable "parent_zone" { }
+
+variable "tags" {
+  type    = map
+  default = { }
+}
+
+resource "azurerm_dns_zone" "z" {
+  name                = var.name
+  resource_group_name = var.resource_group
+  tags                = var.tags
+}
+
+resource "azurerm_dns_ns_record" "zns" {
+  name                = var.name
+  zone_name           = var.parent_zone
+  resource_group_name = var.resource_group
+  ttl                 = 3600
+
+  records = azurerm_dns_zone.z.name_servers 
+
+  tags = var.tags
+}
+
+output "name_servers" {
+  value = azurerm_dns_zone.z.name_servers
+}
+
+output "zone_id" {
+  value = azurerm_dns_zone.z.name
+}
+
+output "fqdn" {
+  value = azurerm_dns_ns_record.zns.fqdn
+}
